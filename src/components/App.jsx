748ef21214d465a04a83d707cac434ad3c36143b
mememaker201 <43540379+mememaker201@users.noreply.github.com>
2021-02-08 17:55:14 -0500

    Did 5 steps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,15 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  var points = 0;
+  return (
+    <div className="app">
+      Trivia!
+      <Question show="Question goes here" />
+    </div>
+  );
 }
-
+function Question(props) {
+  return <div>{props.show}</div>;
+}
 export default App;
